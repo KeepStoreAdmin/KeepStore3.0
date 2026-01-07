f0c2dbc47a6ffe6b51577df17ff233487edff4f2
KeepStoreAdmin <195168582+KeepStoreAdmin@users.noreply.github.com>
2026-01-07 22:54:42 +0100

    Update ISeoMaster.vb

--- App_Code/ISeoMaster.vb
+++ App_Code/ISeoMaster.vb
@@ -1,7 +1,7 @@
 Option Strict On
 Option Explicit On
 
-' Interfaccia minima: il MasterPage espone un payload JSON-LD (iniettato in <head>).
+' Master contract (Page.master.vb può implementarlo)
 Public Interface ISeoMaster
     Property SeoJsonLd As String
 End Interface
