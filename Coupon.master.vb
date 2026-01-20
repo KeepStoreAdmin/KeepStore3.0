cc497c6542b9a900f9d56285ac2101e4d30859ab
KeepStoreAdmin <195168582+KeepStoreAdmin@users.noreply.github.com>
2026-01-20 19:58:54 +0100

    Add files via upload

--- Coupon.master.vb
+++ Coupon.master.vb
@@ -1,34 +1,24 @@
 Partial Class Coupon
-    Protected cont_settori As Integer = 0
     Inherits System.Web.UI.MasterPage
     Implements ISeoMaster
 
-    Private _seoJsonLd As String = ""
+    ' (Master dedicata ai flussi Coupon)
+    Protected cont_settori As Integer = 0
 
     Public Property SeoJsonLd As String Implements ISeoMaster.SeoJsonLd
-        Get
-            Return _seoJsonLd
-        End Get
-        Set(value As String)
-            _seoJsonLd = If(value, "")
-            If litSeoJsonLd IsNot Nothing Then
-                litSeoJsonLd.Text = _seoJsonLd
-            End If
-        End Set
-    End Property
 
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
         If Not Page.IsPostBack Then
-            If litYear IsNot Nothing Then
-                litYear.Text = DateTime.Now.Year.ToString()
-            End If
+            SeoJsonLd = Nothing
         End If
     End Sub
 
     Protected Sub Page_PreRender(sender As Object, e As EventArgs) Handles Me.PreRender
-        If litSeoJsonLd IsNot Nothing Then
-            litSeoJsonLd.Text = SeoJsonLd
+        If Not String.IsNullOrEmpty(SeoJsonLd) Then
+            ' NB: qui si inietta il JSON-LD nel <head> del master Coupon
+            litSeoJsonLd.Text = "<script type=""application/ld+json"">" & SeoJsonLd & "</script>"
+        Else
+            litSeoJsonLd.Text = ""
         End If
     End Sub
-
 End Class
