00cebfe4459673692240aa69418b3fb41da15934
KeepStoreAdmin <195168582+KeepStoreAdmin@users.noreply.github.com>
2026-02-19 00:06:50 +0100

    Delete articoli.security.vb

--- articoli.security.vb
+++
@@ -1,4 +0,0 @@
-' Questo file è mantenuto solo per compatibilità / storicità.
-' NOTA: in un progetto ASP.NET Web Site, i metodi richiamati dal data-binding (<%# ... %>)
-' devono essere presenti nel CodeFile della pagina (articoli.aspx.vb) per essere risolti in compilazione.
-' Le utility WhatsApp sono state quindi spostate in articoli.aspx.vb.
