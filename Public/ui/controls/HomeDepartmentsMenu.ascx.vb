acdda1cf2bc361e0486940a63823fb018e0fac49
KeepStoreAdmin <195168582+KeepStoreAdmin@users.noreply.github.com>
2026-03-21 17:40:59 +0100

    Update HomeDepartmentsMenu.ascx.vb

--- Public/ui/controls/HomeDepartmentsMenu.ascx.vb
+++ Public/ui/controls/HomeDepartmentsMenu.ascx.vb
@@ -6,14 +6,29 @@ Imports System.Web.UI.WebControls
 Partial Public Class UI_HomeDepartmentsMenu
     Inherits UserControl
 
+    ''' <summary>
+    ''' Encodes a value for safe HTML output.
+    ''' </summary>
+    ''' <param name="value">The value to encode.</param>
+    ''' <returns>A HTML-encoded string.</returns>
     Protected Function SafeText(ByVal value As Object) As String
         Return HttpUtility.HtmlEncode(Convert.ToString(value))
     End Function
 
+    ''' <summary>
+    ''' Builds a URL to the articles page for a given sector (settore).
+    ''' </summary>
+    ''' <param name="settoreId">The sector ID.</param>
+    ''' <returns>A URL string.</returns>
     Protected Function BuildSettoreUrl(ByVal settoreId As Object) As String
         Return "articoli.aspx?st=" & Convert.ToString(settoreId)
     End Function
 
+    ''' <summary>
+    ''' Returns the low-resolution image URL for a sector.  If no image is provided,
+    ''' a default banner is used.  This version prepends an underscore to the
+    ''' filename to indicate the smaller size.
+    ''' </summary>
     Protected Function GetSettoreImageLowUrl(ByVal value As Object) As String
         Dim fileName As String = Convert.ToString(value)
         If String.IsNullOrWhiteSpace(fileName) Then
@@ -24,6 +39,10 @@ Partial Public Class UI_HomeDepartmentsMenu
         Return ResolveUrl("~/Public/assets/images/articoli/_" & fileName)
     End Function
 
+    ''' <summary>
+    ''' Returns the normal (high-resolution) image URL for a sector.  If no image is
+    ''' provided, a default banner is used.
+    ''' </summary>
     Protected Function GetSettoreImageNormalUrl(ByVal value As Object) As String
         Dim fileName As String = Convert.ToString(value)
         If String.IsNullOrWhiteSpace(fileName) Then
